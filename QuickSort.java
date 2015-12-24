1fa8d3e85d2523c470d189b484098ab2a1634451
unknown <vijesh568@gmail.com>
2015-12-24 18:13:53 +0530

    merge and quick sort added

--- QuickSort.java
+++ QuickSort.java
@@ -1,9 +1,9 @@
 
-public class MergeSort {
+public class QuickSort {
 	static Integer[] array = new Integer[] {7,10,5,3,8,4,2,9,6,14,0,1,15,12,13,11};
 	
 	public static void main(String[] args) {
-		MergeSort sort = new MergeSort();
+		QuickSort sort = new QuickSort();
 		sort.print();
 		sort.sort();
 		sort.print();
@@ -38,46 +38,45 @@ public class MergeSort {
 	}
 
 	public void sort() {
-		Comparable<Integer>[] aux = new Comparable[array.length];
-		sort(array, aux, 0, array.length-1);
+		sort(array, 0, array.length-1);
 	}
 	
-	private void sort(Comparable<Integer>[] arr, Comparable<Integer>[] aux, int lo, int hi) {
-		if (lo>=hi) {
+	private void sort(Comparable<Integer>[] a, int lo, int hi) {
+		if (lo>=hi)
 			return;
-		}
-		int mid = lo + (hi-lo)/2;
-		sort(arr, aux, lo, mid);
-		sort(arr, aux, mid+1, hi);
-//		if (!less(arr[mid+1],arr[mid])) return;
-		merge(arr, aux, lo, mid, hi);
+		
+		int k = partition(a, lo, hi);
+		sort(a, lo, k-1);
+		sort(a, k+1, hi);
 	}
 	
 	
-	private void merge(Comparable<Integer>[] arr, Comparable<Integer>[] aux, int lo, int mid, int hi) {
-		for (int i = lo; i <= hi; i++) {
-			aux[i] = arr[i];
-		}
-		int i = lo, j=mid+1;
-		for (int k = lo; k <= hi; k++) {
-			if (i>mid) {
-				arr[k] = aux[j++];
-			} else if (j>hi){
-				arr[k] = aux[i++];
-			} else if (less(aux[i], aux[j])) {
-				arr[k] = aux[i++];
+	private int partition(Comparable<Integer>[] a, int lo, int hi) {
+		int i= lo, j = hi+1;
+		
+		while(true) {
+			while(less(a[++i],a[lo])) {
+				if (i>=hi) break;
+			}
+			while(less(a[lo],a[--j])) {
+				if(j<=lo) break;
+			}
+			if (i<j) {
+				swap(a, i, j);
 			} else {
-				arr[k] = aux[j++];
+				break;
 			}
 		}
+		swap(a,j,lo);
+		return j;
 	}
 	
 	private boolean less(Comparable<Integer> i, Comparable<Integer> j) {
 		return ((i.compareTo((Integer) j))<0);
 	}
 	
-	private void swap(Integer[] array, int i, int j) {
-		Integer temp = array[i];
+	private void swap(Comparable<Integer>[] array, int i, int j) {
+		Comparable<Integer> temp = array[i];
 		array[i] = array[j];
 		array[j] = temp;
 	}
--- QuickSort_3_way.java
+++ QuickSort_3_way.java
@@ -1,9 +1,9 @@
 
-public class MergeSort {
+public class QuickSort_3_way {
 	static Integer[] array = new Integer[] {7,10,5,3,8,4,2,9,6,14,0,1,15,12,13,11};
 	
 	public static void main(String[] args) {
-		MergeSort sort = new MergeSort();
+		QuickSort_3_way sort = new QuickSort_3_way();
 		sort.print();
 		sort.sort();
 		sort.print();
@@ -38,46 +38,37 @@ public class MergeSort {
 	}
 
 	public void sort() {
-		Comparable<Integer>[] aux = new Comparable[array.length];
-		sort(array, aux, 0, array.length-1);
+		sort(array, 0, array.length-1);
 	}
 	
-	private void sort(Comparable<Integer>[] arr, Comparable<Integer>[] aux, int lo, int hi) {
-		if (lo>=hi) {
+	private void sort(Comparable<Integer>[] a, int lo, int hi) {
+		if (lo>=hi)
 			return;
-		}
-		int mid = lo + (hi-lo)/2;
-		sort(arr, aux, lo, mid);
-		sort(arr, aux, mid+1, hi);
-//		if (!less(arr[mid+1],arr[mid])) return;
-		merge(arr, aux, lo, mid, hi);
-	}
-	
-	
-	private void merge(Comparable<Integer>[] arr, Comparable<Integer>[] aux, int lo, int mid, int hi) {
-		for (int i = lo; i <= hi; i++) {
-			aux[i] = arr[i];
-		}
-		int i = lo, j=mid+1;
-		for (int k = lo; k <= hi; k++) {
-			if (i>mid) {
-				arr[k] = aux[j++];
-			} else if (j>hi){
-				arr[k] = aux[i++];
-			} else if (less(aux[i], aux[j])) {
-				arr[k] = aux[i++];
-			} else {
-				arr[k] = aux[j++];
+		int i = lo+1, gt = hi, lt = lo;
+		while(i<=gt) {
+			if (less(a[i],a[lt])) {
+				swap(a, i, lt);
+				lt++;i++;
+			} else if (a[i].equals(a[lt])) {
+				i++;
+			}else {
+				swap(a, i, gt);
+				gt--;
 			}
 		}
+		
+		
+		sort(a, lo, lt-1);
+		sort(a, i, hi);
 	}
+	
 	
 	private boolean less(Comparable<Integer> i, Comparable<Integer> j) {
 		return ((i.compareTo((Integer) j))<0);
 	}
 	
-	private void swap(Integer[] array, int i, int j) {
-		Integer temp = array[i];
+	private void swap(Comparable<Integer>[] array, int i, int j) {
+		Comparable<Integer> temp = array[i];
 		array[i] = array[j];
 		array[j] = temp;
 	}
